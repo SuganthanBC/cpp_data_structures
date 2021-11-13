c770da5e6d25b4ca8d84f82c92c5f3a4aba2736f
Suganthan <bc.suganth@gmail.com>
2021-11-14 00:22:23 +0530

    Changed blockStart() to shouldUpdate(). The processor can hold on to its sharedData until it decides to pull()
    The RealTime's version of the sharedData can be accessed directly without having to make a copy of it since it can hold on to it until it decides to pull()
    
    This is only useful for cases in which the processor has to depend on a snapshot of the GUI's sharedData between say, a keyswitch noteOn and a keyswitch noteOff.
    
    If a GUI push is available when the keyswitch is active, the processor, at the start of the block should synthesize a keyswitch noteOff, pull the latest sharedData, then retrigger the same keyswitch note.

--- ea_data_structures/Structures/Fifo.h
+++ ea_data_structures/Structures/Fifo.h
@@ -12,12 +12,14 @@ namespace EA
  * pointer to use from a specific thread that remains untouched
  * until the newxt pull() call.
  *
- * This FIFO is meant for cases where we want the most up-to-date
- * version of the shared object when reading from it, but we're ok with 'skipping'
- * some of the in-between updates.
+ * This FIFO is meant for cases where we want the processor to have a version of the shared object 
+ * independent from the GUI processing/writes and pushes. At the same time, if a push from the GUI is available,
+ * the processor will know it by calling shouldUpdate(). After which, depending on the shouldUpdate() result, 
+ * the processor will prepare and then updates its version of the shared object by calling pull()
  *
  * This is good for something like a big vector that the processor needs to know about
- * constantly without knowing exactly what changed.
+ * constantly and also if any pushes from GUI is available, it has to note off the current sounding midi notes,
+ * get the pointer to the recently pushed big vector, retrigger those notes
  *
  * For message passing between threads where you absolutely need every message read,
  * probably something else is useful
@@ -30,6 +32,9 @@ public:
     {
         auto loc = writePos.load();
         array[loc] = object;
+        
+        /* After writing the object, the position is saved for future realtime reads */        
+        futureReadPos.store(loc);
 
         do
         {
@@ -41,22 +46,19 @@ public:
 
     T& pull()
     {
-        auto readPos = getReadLocation();
+        /*   
+              
+        No calls to pull() means the processor's reading data will be intact
+        a single call to pull() = guaranteed latest data position that has been pushed by the GUI 
+
+        */
+        
+        auto readPos = futureReadPos.load() ;
         currentReadPosition.store(readPos);
         return array[readPos];
     }
 
 private:
-    int getReadLocation()
-    {
-        auto readPos = writePos.load() - 1;
-
-        if (readPos == -1)
-            readPos = array.getLastElementIndex();
-
-        return readPos;
-    }
-
     void getNextLocation(int& prevLocation)
     {
         prevLocation++;
@@ -65,8 +67,9 @@ private:
             prevLocation = 0;
     }
 
-    std::atomic<int> writePos {0};
-    std::atomic<int> currentReadPosition {-1};
+    std::atomic<int> writePos {1};
+    std::atomic<int> currentReadPosition {0};
+    std::atomic<int> futureReadPos {0};
 
     Array<T, size> array;
 };
--- ea_data_structures/Structures/SharedGUIData.h
+++ ea_data_structures/Structures/SharedGUIData.h
@@ -4,15 +4,36 @@
 
 namespace EA
 {
-/*This is a useful object for classic cases of sharing random data
- * that gets edited in the GUI but read from the processor
+/* This is a useful object for classic cases of sharing random data
+ * that gets edited in the GUI but read from the processor and 
+ * given the processor has to prepare(do some processing to let go of the old version of shared data)
+ * inorder to get the updates from the GUI
  *
  * To use, create one SharedWithRealTime<T> in your processor. You can freely pass
- * a reference to it, but remember that you must call push() for the updates
+ * a reference to it, but remember that you must call push() at the end of the GUI processing for the updates
  * to register in the processor
  *
- * On the processor side, you need to call blockStarted() at the start of the block
+ * On the processor side, you need to call shouldUpdate() at the start of the block. 
+ * If it is true, do the preparations to get the latest GUI data push, and then call getRealTime()
  * And from then on the rt object will be safe to use with no thread contentions
+
+Example:
+========
+
+void MainProcessor::processBlock()
+{
+    if (sharedState.shouldUpdate()) //Is there any updates from the GUI?
+    {
+        handleNoteOff(MidiMessage::noteOff(1, currentNote));  //Turn the current notes off
+
+        rtData = &sharedState.getRealTime();  //Update RT data 
+
+        handleNoteOn(MidiMessage::noteOn(1, currentNote, velocity)); //Trigger the same notes again
+    }  
+
+    ...  
+}
+
 */
 template <typename T, int fifoSize = 5>
 class SharedWithRealTime
@@ -20,11 +41,18 @@ class SharedWithRealTime
 public:
     SharedWithRealTime() { push(); }
 
-    //Call at the start of the block. This will make the RT pointer safe to use
-    void blockStarted() { rt = &fifo.pull(); }
+    //Call at the start of the block in the processor to decide whether to prepare and pull or not
+    bool shouldUpdate() 
+    { 
+        return shouldProcessorUpdate.load();
+    }
 
     //Call this from the GUI thread to update the data
-    void push() { fifo.push(data); }
+    void push() 
+    { 
+        fifo.push(data); 
+        shouldProcessorUpdate.store(true);
+    }
 
     //Useful operators to call from the GUI only to reach the shared object
     T* operator->() { return &data; }
@@ -33,16 +61,22 @@ public:
     //Another way to access the realtime object. Only valid during processBlock.
     T& getRealTime()
     {
-        blockStarted();
+        shouldProcessorUpdate.store(false);
+        rt = &fifo.pull();        
         return *rt;
     }
 
-    //Safe to use pointer, but only during the process block. See blockStarted()
+    //Safe to use pointer, but only during the process block.
     T* rt = nullptr;
 
 private:
+    //GUI read and GUI writes
     T data;
+
+    //RT pulls and GUI pushes
     Fifo<T, fifoSize> fifo;
+
+    std::atomic<bool> shouldProcessorUpdate;
 };
 
 //Similar to SharedWithRealTime, but a non-owning version of the GUI data
